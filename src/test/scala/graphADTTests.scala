515e70710a9fdc2e155b34ad81c1716cf7b4066e
Miguel Iglesias <caente@gmail.com>
2016-12-27 23:25:18 -0500

    not working at all, but the tests seem OK

--- src/test/scala/graphADTTests.scala
+++ src/test/scala/graphADTTests.scala
@@ -6,6 +6,13 @@ import org.scalactic.TypeCheckedTripleEquals
 import graph2._
 
 class GraphTests extends FunSuite with TypeCheckedTripleEquals {
+
+  test("Besides before Single") {
+    implicit val relation: Relation[Int] = (x, y) => x > y && y % 2 == 0
+    assert(
+      (Single(3) append (Single(1) append Single(2))) === Besides(Before(Single(3), Single(2)), Single(1))
+    )
+  }
   val nodes = Map(
     1 -> List(3),
     2 -> List(3),
@@ -14,6 +21,7 @@ class GraphTests extends FunSuite with TypeCheckedTripleEquals {
     5 -> List(6),
     6 -> List()
   )
+
   /*
  
 1 ---|
@@ -31,71 +39,90 @@ class GraphTests extends FunSuite with TypeCheckedTripleEquals {
   implicit val relation: Relation[Int] = (x, y) => nodes(x).contains(y)
 
   test("add to empty") {
-    DAG.empty[Int].append(Single(1)) === Single(1)
+    assert(
+      DAG.empty[Int].append(Single(1)) === Single(1)
+    )
   }
 
   test("add related to Single") {
-    DAG.empty[Int]
-      .append(Single(1))
-      .append(Single(2)) === Besides(Single(1), Single(2))
+    assert(
+      Single(1)
+        .append(Single(2)) === Besides(Single(1), Single(2))
+    )
   }
 
   test("add related to Besides") {
-    DAG.empty[Int]
-      .append(Single(1))
-      .append(Single(2))
-      .append(Single(3)) === Before(Besides(Single(1), Single(2)), Single(3))
+    assert(
+      Single(1)
+        .append(Single(2))
+        .append(Single(3)) === Before(Besides(Single(1), Single(2)), Single(3))
+    )
+  }
+
+  test("add Besides to Single") {
+    assert(
+      Single(3)
+        .append(Single(1))
+        .append(Single(2)) === Before(Besides(Single(1), Single(2)), Single(3))
+    )
   }
 
   test("add non-related to Before") {
-    DAG.empty[Int]
-      .append(Single(1))
-      .append(Single(2))
-      .append(Single(3))
-      .append(Single(4)) === Besides(Before(Besides(Single(1), Single(2)), Single(3)), Single(4))
+    assert(
+      Single(1)
+        .append(Single(2))
+        .append(Single(3))
+        .append(Single(4)) === Besides(Before(Besides(Single(1), Single(2)), Single(3)), Single(4))
+    )
   }
 
   test("add partially-related to Besides") {
-    DAG.empty[Int]
-      .append(Single(1))
-      .append(Single(2))
-      .append(Single(3))
-      .append(Single(4))
-      .append(Single(5)) === Besides(Before(Before(Besides(Single(1), Single(2)), Single(3)), Single(5)), Single(4))
+    assert(
+      Single(1)
+        .append(Single(2))
+        .append(Single(3))
+        .append(Single(4))
+        .append(Single(5)) === Besides(Before(Before(Besides(Single(1), Single(2)), Single(3)), Single(5)), Single(4))
+    )
   }
 
   test("add double-related to Besides") {
-    DAG.empty[Int]
-      .append(Single(1))
-      .append(Single(2))
-      .append(Single(3))
-      .append(Single(4))
-      .append(Single(5))
-      .append(Single(6)) === Before(Besides(Before(Before(Besides(Single(1), Single(2)), Single(3)), Single(5)), Single(4)), Single(6))
+    assert(
+      Single(1)
+        .append(Single(2))
+        .append(Single(3))
+        .append(Single(4))
+        .append(Single(5))
+        .append(Single(6)) === Before(Besides(Before(Before(Besides(Single(1), Single(2)), Single(3)), Single(5)), Single(4)), Single(6))
+    )
   }
 
   test("root") {
-    DAG.empty[Int]
-      .append(Single(1))
-      .append(Single(2))
-      .append(Single(3))
-      .append(Single(4))
-      .append(Single(5))
-      .append(Single(6))
-      .root === Besides(Besides(Single(1), Single(2)), Single(4))
+    assert(
+      Single(1)
+        .append(Single(2))
+        .append(Single(3))
+        .append(Single(4))
+        .append(Single(5))
+        .append(Single(6))
+        .root === Besides(Besides(Single(1), Single(2)), Single(4))
+    )
   }
 
   test("leaf") {
-    DAG.empty[Int]
-      .append(Single(1))
-      .append(Single(2))
-      .append(Single(3))
-      .append(Single(4))
-      .append(Single(5))
-      .append(Single(6))
-      .leaf === Single(6)
+    assert(
+      Single(1)
+        .append(Single(2))
+        .append(Single(3))
+        .append(Single(4))
+        .append(Single(5))
+        .append(Single(6))
+        .leaf === Single(6)
+    )
   }
   test("generator") {
-    DAG(List(1, 2, 3, 4, 5, 6)) === Before(Besides(Before(Before(Besides(Single(1), Single(2)), Single(3)), Single(5)), Single(4)), Single(6))
+    assert(
+      DAG(List(1, 2, 3, 4, 5, 6)) === Before(Besides(Before(Before(Besides(Single(1), Single(2)), Single(3)), Single(5)), Single(4)), Single(6))
+    )
   }
 }
